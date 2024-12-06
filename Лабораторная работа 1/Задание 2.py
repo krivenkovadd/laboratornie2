4552e093a8e149c5061cf653ebb936061f72cd15
krivenkovadd <krivenkovadd@gmail.com>
2024-12-06 15:38:08 +0300

    Add files via upload

---
+++ Лабораторная работа 1/Задание 2.py
@@ -0,0 +1,21 @@
+from task import Book, MiniCooper, BankAccount
+
+if __name__ == "__main__":
+ book = Book('Мастер и Маргарита', 'Михаил Булгаков', 512)
+ mini_cooper = MiniCooper('Cooper S', 2022, 40.0, 'Красный')
+ bank_account = BankAccount('100500', 'Кривенкова Дарья', 1000.0)
+
+    try:
+     book.set_pages(-100)
+    except TypeError:
+        print('Ошибка: неправильные данные')
+
+    try:
+     mini_cooper.refuel(-20.0)
+    except TypeError:
+        print('Ошибка: неправильные данные')
+
+    try:
+     bank_account.withdraw(-500.0)
+    except TypeError:
+        print('Ошибка: неправильные данные')
