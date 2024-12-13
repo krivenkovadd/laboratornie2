e67ff102fc5fa186bedc03208025b460b6a43259
krivenkovadd <krivenkovadd@gmail.com>
2024-12-13 22:44:33 +0300

    Update Задание 2.py

--- Лабораторная работа 1/Задание 2.py
+++ Лабораторная работа 1/Задание 2.py
@@ -1,21 +1,23 @@
-from task import Book, MiniCooper, BankAccount
+from task_1 import Book, MiniCooper, BankAccount
+
 
 if __name__ == "__main__":
- book = Book('Мастер и Маргарита', 'Михаил Булгаков', 512)
- mini_cooper = MiniCooper('Cooper S', 2022, 40.0, 'Красный')
- bank_account = BankAccount('100500', 'Кривенкова Дарья', 1000.0)
+    book = Book('Мастер и Маргарита', 'Михаил Булгаков', 512)
+    mini_cooper = MiniCooper('Cooper S', 2022, 40.0, 'Красный')
+    bank_account = BankAccount('100500', 'Кривенкова Дарья', 1000.0)
 
     try:
-     book.set_pages(-100)
-    except TypeError:
+        book.set_pages(-100)
+    except ValueError:
         print('Ошибка: неправильные данные')
 
     try:
-     mini_cooper.refuel(-20.0)
-    except TypeError:
+        mini_cooper.refuel(-20.0)
+    except ValueError:
         print('Ошибка: неправильные данные')
 
     try:
-     bank_account.withdraw(-500.0)
-    except TypeError:
+        bank_account.withdraw(-500.0)
+    except ValueError:
         print('Ошибка: неправильные данные')
+     
